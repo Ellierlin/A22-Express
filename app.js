18e7e3532694e2c3e7787f93b8713729076fb575
Ellie Lin <ellielin790108@gmail.com>
2023-05-17 18:26:18 +0800

    Update app.js
    add about

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
 app.listen(port, () => {
     console.log(`Express is running on http://localhost:${port}`)
 })
