5ba1f59308710503047a5bf893fe6f5191206ca0
Ellie Lin <ellielin790108@gmail.com>
2023-05-19 15:42:24 +0800

    Update app.js
    create default layout to be 'main'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
-app.engine('handlebars', exphbs())
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
